4c9086c313e2dd29dac5303d0ddf10c96ca5d6c4
astar1ka <astar1ka@mail.ru>
2022-11-09 00:37:06 +0400

    Update feodal.sql

--- api/config/db/feodal.sql
+++ api/config/db/feodal.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1:3306
--- Время создания: Ноя 04 2022 г., 20:53
+-- Время создания: Ноя 08 2022 г., 23:35
 -- Версия сервера: 8.0.30
 -- Версия PHP: 7.2.34
 
@@ -32,8 +32,8 @@ CREATE TABLE `gamers` (
   `userId` int NOT NULL,
   `money` int NOT NULL DEFAULT '1000',
   `castleLevel` int NOT NULL DEFAULT '1',
-  `castleX` varchar(256) NOT NULL,
-  `castleY` varchar(256) NOT NULL
+  `castleX` double NOT NULL,
+  `castleY` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
@@ -41,7 +41,8 @@ CREATE TABLE `gamers` (
 --
 
 INSERT INTO `gamers` (`id`, `userId`, `money`, `castleLevel`, `castleX`, `castleY`) VALUES
-(3, 2, 1000, 1, '1', '1');
+(4, 3, 1000, 1, 1, 1),
+(5, 2, 661, 1, 40.411, 1.687);
 
 -- --------------------------------------------------------
 
@@ -79,7 +80,8 @@ INSERT INTO `messages` (`id`, `userId`, `message`, `messageTo`) VALUES
 (10, 2, 'ghbdtn', NULL),
 (11, 2, 'ghbdtn', NULL),
 (12, 2, 'ghbdtn', NULL),
-(13, 2, '765233765234', NULL);
+(13, 2, '765233765234', NULL),
+(14, 2, 'Привет', NULL);
 
 -- --------------------------------------------------------
 
@@ -100,7 +102,7 @@ CREATE TABLE `statuses` (
 --
 
 INSERT INTO `statuses` (`id`, `chatHash`, `mapHash`, `unitsHash`, `mapTimeStamp`) VALUES
-(1, '1c59238c623f34cfe3a0d4fdbcffb595', '77a7837e9e5f790d170b79b55e64dc2d', 'c3bd042044ed6cd0ea3767e23435ff29', '');
+(1, '1b775eae1a2e078b504ed597b57a1357', '8264cea6fac62353407447f366f7837e', '36316421dbf4dd08d47f5a96f8ba187f', '1667939687');
 
 -- --------------------------------------------------------
 
@@ -116,7 +118,7 @@ CREATE TABLE `units` (
   `posX` double NOT NULL DEFAULT '0',
   `posY` double DEFAULT '0',
   `status` varchar(256) NOT NULL DEFAULT 'inCastle',
-  `direction` varchar(256) NOT NULL
+  `direction` varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
@@ -124,17 +126,25 @@ CREATE TABLE `units` (
 --
 
 INSERT INTO `units` (`id`, `type`, `gamerId`, `hp`, `posX`, `posY`, `status`, `direction`) VALUES
-(33, 1, 2, 10, 0, 0, 'inCastle', ''),
-(34, 1, 2, 10, 0, 0, 'inCastle', ''),
-(35, 1, 2, 10, 0, 0, 'inCastle', '');
+(36, 1, 4, 10, 1, 1, 'inCastle', ''),
+(37, 1, 4, 10, 1, 1, 'inCastle', ''),
+(39, 1, 4, 10, 0, 0, 'inCastle', NULL),
+(40, 1, 4, 10, 1, 1, 'inCastle', NULL),
+(41, 1, 5, 10, 0, 0, 'inCastle', NULL),
+(42, 1, 5, 10, 0, 0, 'inCastle', NULL),
+(43, 1, 5, 10, 0, 0, 'inCastle', NULL),
+(44, 1, 5, 10, 40.411, 1.687, 'inCastle', NULL),
+(45, 1, 5, 10, 40.411, 1.687, 'inCastle', NULL),
+(46, 1, 5, 10, 40.411, 1.687, 'inCastle', NULL),
+(47, 1, 5, 10, 40.411, 1.687, 'inCastle', NULL);
 
 -- --------------------------------------------------------
 
 --
--- Структура таблицы `unittypes`
+-- Структура таблицы `unitstypes`
 --
 
-CREATE TABLE `unittypes` (
+CREATE TABLE `unitstypes` (
   `id` int NOT NULL,
   `name` varchar(256) NOT NULL,
   `hp` int NOT NULL,
@@ -144,10 +154,10 @@ CREATE TABLE `unittypes` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci ROW_FORMAT=COMPACT;
 
 --
--- Дамп данных таблицы `unittypes`
+-- Дамп данных таблицы `unitstypes`
 --
 
-INSERT INTO `unittypes` (`id`, `name`, `hp`, `cost`, `damage`, `speed`) VALUES
+INSERT INTO `unitstypes` (`id`, `name`, `hp`, `cost`, `damage`, `speed`) VALUES
 (1, 'soldier', 10, 100, 1, 0);
 
 -- --------------------------------------------------------
@@ -170,7 +180,7 @@ CREATE TABLE `users` (
 
 INSERT INTO `users` (`id`, `login`, `password`, `name`, `token`) VALUES
 (1, 'vasya', '12345', 'Vasya Pupkin', NULL),
-(2, 'admin', 'admin', 'Administrator', '');
+(2, 'admin', 'admin', 'Administrator', 'd5752a359f4c715657f4b68155066342');
 
 -- --------------------------------------------------------
 
@@ -181,12 +191,23 @@ INSERT INTO `users` (`id`, `login`, `password`, `name`, `token`) VALUES
 CREATE TABLE `villages` (
   `id` int NOT NULL,
   `name` varchar(256) NOT NULL,
-  `level` int NOT NULL,
-  `population` int NOT NULL,
+  `level` int NOT NULL DEFAULT '1',
+  `population` int NOT NULL DEFAULT '10',
   `posX` double NOT NULL,
   `posY` double NOT NULL,
-  `money` int NOT NULL
+  `money` int NOT NULL DEFAULT '100'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Дамп данных таблицы `villages`
+--
+
+INSERT INTO `villages` (`id`, `name`, `level`, `population`, `posX`, `posY`, `money`) VALUES
+(3, 'Верхние Потёмки', 2, 10, 1, 1, -565),
+(8, 'Далёкие Удалёнки', 1, 10, 0.429, 29.268, 180),
+(10, 'Болотистые Разгромки', 1, 10, 9.95, 72.214, 140),
+(11, 'Нижние Свистульки', 1, 10, 116.092, 77.843, 120),
+(12, 'Нижние Удалёнки', 1, 10, 43.592, 116.777, 100);
 
 --
 -- Индексы сохранённых таблиц
@@ -220,12 +241,13 @@ ALTER TABLE `statuses`
 -- Индексы таблицы `units`
 --
 ALTER TABLE `units`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `gamerId` (`gamerId`);
 
 --
--- Индексы таблицы `unittypes`
+-- Индексы таблицы `unitstypes`
 --
-ALTER TABLE `unittypes`
+ALTER TABLE `unitstypes`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -249,7 +271,7 @@ ALTER TABLE `villages`
 -- AUTO_INCREMENT для таблицы `gamers`
 --
 ALTER TABLE `gamers`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT для таблицы `maps`
@@ -261,7 +283,7 @@ ALTER TABLE `maps`
 -- AUTO_INCREMENT для таблицы `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT для таблицы `statuses`
@@ -273,12 +295,12 @@ ALTER TABLE `statuses`
 -- AUTO_INCREMENT для таблицы `units`
 --
 ALTER TABLE `units`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=48;
 
 --
--- AUTO_INCREMENT для таблицы `unittypes`
+-- AUTO_INCREMENT для таблицы `unitstypes`
 --
-ALTER TABLE `unittypes`
+ALTER TABLE `unitstypes`
   MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
@@ -291,7 +313,17 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT для таблицы `villages`
 --
 ALTER TABLE `villages`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `units`
+--
+ALTER TABLE `units`
+  ADD CONSTRAINT `units_ibfk_1` FOREIGN KEY (`gamerId`) REFERENCES `gamers` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
