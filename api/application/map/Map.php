6973097d8999ca0936e7dab8f32cabe14c7d3c40
astar1ka <astar1ka@mail.ru>
2022-11-15 21:39:11 +0400

    1.2

--- api/application/map/Map.php
+++ api/application/map/Map.php
@@ -7,19 +7,13 @@ class Map{
         $this->test = "test";
     }
 
-    public function getMap() {
-        return json_decode($this->db->getMap(), true);
-        /*
-        return $array;
-        return $object = array(
-            'layer1' => $array[0],
-            'layer2' => $array[1],
-            'layer3' => $array[2] 
-        );*/
+    private function getMap($mapId) {
+        $map = $this->db->getMap($mapId);
     }
 
     public function validPosObject($mapId, $posX, $poxY, $type){
-
+        $map = $this->getMap($mapId);
+        return true;
     }
 
 }
