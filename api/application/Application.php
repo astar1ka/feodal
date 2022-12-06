2c07f5740865057dc220772868d3ba5fbe8dab10
astar1ka <33550716+astar1ka@users.noreply.github.com>
2022-12-06 14:07:37 +0400

    Villages

--- api/application/Application.php
+++ api/application/Application.php
@@ -236,7 +236,7 @@ class Application {
             if  ($userId){
                 $gamer = $this->gamer->getGamer($userId);
                 if ($gamer) {
-                    $time = $this->gamer->updateUnits($gamer, $data->myUnits, $data->otherUnits, $params['villages']);
+                    $time = $this->gamer->updateUnits($gamer, $data->myUnits, $data->otherUnits, $data->villages);
                     if ($time) {
                         $this->game->updateMap($time);
                     }
